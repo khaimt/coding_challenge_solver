d77660c38abb034a4e23bbadc4b06d94cba20069
Hung Nguyen <hungmanh.hust@gmail.com>
2024-01-07 00:10:59 +0700

    crawl from leetcode

--- run_inference.py
+++ run_inference.py
@@ -2,17 +2,50 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from prompt_template import get_prompt_template_by_name
 import torch
 import typer
+import requests
+from bs4 import BeautifulSoup
 
 
-def main(input_file: str, model_path: str = typer.Option("khaimaitien/leetcode_solver_7b")):
+def crawl_desc_leetcode_url(url: str) -> str:
+    """Crawl algorithm description from leetcode"""
+    if "leetcode.com" not in url:
+        raise ValueError(
+            f"Please input url to leetcode problem. "
+            f"For example: 'https://leetcode.com/problems/regular-expression-matching/")
+    response = requests.get(url)
+    response.raise_for_status()
+    response.encoding = "utf-8"
+    html = response.text
+    if html:
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Find the meta tag with name="description"
+        meta_description = soup.find('meta', attrs={'name': 'description'})
+
+        # Extract the content attribute value
+        if meta_description:
+            content_value = meta_description.get('content')
+            return content_value
+        else:
+            return None
+    else:
+        return None
+
+
+def main(url: str, model_path: str = typer.Option("khaimaitien/leetcode_solver_7b")):
     prompt_template = get_prompt_template_by_name("code_llama")
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", torch_dtype=torch.bfloat16)
 
     # read the leetcode problem from input_file
-    with open(input_file, "r") as f:
-        algo_input = f.read()
+    # with open(input_file, "r") as f:
+    #     algo_input = f.read()
+
+    algo_input = crawl_desc_leetcode_url(url)
+    if algo_input is None:
+        print("Can not crawl problem from leetcode.")
+        return None
 
     prompt_str = prompt_template.get_prompt_from_inputs({"algo_input": algo_input})
     print("--------prompt-------")
@@ -28,4 +61,6 @@ def main(input_file: str, model_path: str = typer.Option("khaimaitien/leetcode_s
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    # typer.run(main)
+    reader = crawl_desc_leetcode_url("https://aaa.com/problems/substring-with-concatenation-of-all-words/")
+    print(reader)
